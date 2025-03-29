ded85032af6f1304ef8e1e9fa577a3d8cb975698
Rdm-jony <jonydascse@gmail.com>
2025-03-29 22:10:38 +0600

    created heade component

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,8 @@
+import { SignIn } from '@clerk/nextjs';
+import React from 'react';
+
+const SignInRoute = () => {
+    return <SignIn></SignIn>
+};
+
+export default SignInRoute;
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,8 @@
+import { SignUp } from '@clerk/nextjs';
+import React from 'react';
+
+const SignUpRoute = () => {
+    return <SignUp></SignUp>
+};
+
+export default SignUpRoute;
